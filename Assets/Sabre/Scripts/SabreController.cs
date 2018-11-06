e40116b16302b2127c095bfb436c00047afb25e1
unknown <Administrateur@JMIN-D1727.formation.enjmin.fr>
2018-11-06 15:17:39 +0100

    Activation du sabre

---
+++ Assets/Sabre/Scripts/SabreController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using DigitalRuby.LaserSword;
+
+public class SabreController : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        GetComponent<LaserSwordScript>().Activate();
+    }
+    void Update()
+    {
+        //if (
+        {
+        }
+    }
+
+} 
+
